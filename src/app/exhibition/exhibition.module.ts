66c997a917da6d9eb6db83804cc760c1ffdc5064
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-04-24 12:13:52 -0500

    Generate modulo exhibition

---
+++ src/app/exhibition/exhibition.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExhibitionComponent } from './exhibition.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ExhibitionComponent]
+})
+export class ExhibitionModule { }
