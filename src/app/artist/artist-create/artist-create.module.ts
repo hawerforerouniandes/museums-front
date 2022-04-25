075ee08d26d4ee29dfa02a7099499924d4583f37
John Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2022-04-24 22:45:14 -0500

    feat (Artist): Add artist create, detail and list components

---
+++ src/app/artist/artist-create/artist-create.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArtistCreateComponent } from './artist-create.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ArtistCreateComponent]
+})
+export class ArtistCreateModule { }
---
+++ src/app/artist/artist-list/artist-list.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArtistListComponent } from './artist-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ArtistListComponent]
+})
+export class ArtistListModule { }
