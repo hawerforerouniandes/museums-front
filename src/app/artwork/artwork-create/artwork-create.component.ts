023149cf8a1b69b8d6c19ef58bf29bec58063054
John Guerrero Ayala <je.guerreroa1@uniandes.edu.co>
2022-04-24 23:03:20 -0500

    feat (Artwork): Add artwork components to create, detail and list

---
+++ src/app/artwork/artwork-create/artwork-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artwork-create',
+  templateUrl: './artwork-create.component.html',
+  styleUrls: ['./artwork-create.component.css']
+})
+export class ArtworkCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
