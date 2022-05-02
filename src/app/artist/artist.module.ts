7bb2716e531323d0a7077603e6a0b01d473bd6c4
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-05-02 00:23:30 -0500

    Agrega template html

--- src/app/artist/artist.module.ts
+++ src/app/artist/artist.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArtistListComponent } from './artist-list/artist-list.component';
+import { ArtistRoutingModule } from './artist-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ArtistRoutingModule
   ],
   exports: [ArtistListComponent],
   declarations: [ArtistListComponent]
