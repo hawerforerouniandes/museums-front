e58ff1f85ce90646e61a0624fb98f9c8f3425a72
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-05-28 13:25:37 -0500

    test should have an table element

--- src/app/sponsor/sponsor-list/sponsor-list.component.spec.ts
+++ src/app/sponsor/sponsor-list/sponsor-list.component.spec.ts
@@ -2,14 +2,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { SponsorListComponent } from './sponsor-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SponsorService } from '../sponsor.service';
+import { Sponsor } from '../sponsor';
+import { Exhibition } from 'src/app/exhibition/exhibition';
 
 describe('SponsorListComponent', () => {
   let component: SponsorListComponent;
   let fixture: ComponentFixture<SponsorListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -23,10 +27,18 @@ describe('SponsorListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SponsorListComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an table element ', () => {
+    expect(debug.query(By.css('table')).attributes['alt']).toEqual(
+      "Table sponsors"
+    );
   });
 });
