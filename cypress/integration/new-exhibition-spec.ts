812ebdf68a3d9d9d93e332e7a0d8506961ed14ed
hawerforerouniandes <h.foreror@uniandes.edu.co>
2022-05-29 17:52:32 -0500

    test e2e new exhibition

---
+++ cypress/integration/new-exhibition-spec.ts
@@ -0,0 +1,43 @@
+
+describe('My First Test', () => {
+  it('Visits the initial project page', () => {
+
+    //Home
+    cy.visit('/')
+
+    //Open museum
+    cy.get('[alt="Bendigo Art Gallery"]').click();
+    cy.wait(200);
+
+    //List exhibitions
+    cy.contains("Open exhibitions").click();
+    cy.wait(100);
+
+    //New exhibition
+    cy.contains("New exhibit").click();
+    cy.wait(100);
+
+    //Data form
+    let body = cy.get("body");
+    body.find("#name").first().type("Raphael", { force: true });
+    cy.wait(100);
+    body = cy.get("body");
+    body.find("#description").first().type("One of the first-ever exhibitions to explore Raphael's complete career", { force: true });
+    cy.wait(100);
+
+    //Sponsor
+    cy.get('[formcontrolname="sponsor"]').select(0);
+    cy.wait(100);
+
+     //Create exhibicion
+     cy.get('[type="submit"]').click();
+     cy.wait(100);
+
+    //Then
+     cy.contains('Raphael')
+     cy.wait(100);
+
+
+
+  })
+ })
